23df1296b827c98d82e94c879e7a01adc0af9288
Sam Wei <68414598+Samwei1@users.noreply.github.com>
2023-07-18 15:31:18 +0800

    Update parse.py

--- parse.py
+++ parse.py
@@ -7,7 +7,7 @@ def parse_args():
                         help='Choose a dataset')
     parser.add_argument('--modeltype', type=str, default='UNet',
                         help='which model wanna use.')
-    parser.add_argument('--n_class', type=int, default=12,
+    parser.add_argument('--n_class', type=int, default=10,
                         help='no. class')
     parser.add_argument('--lr', type=float, default=1e-3,
                         help='Learning rate.')
@@ -15,7 +15,7 @@ def parse_args():
                         help='Number of epoch.')
     parser.add_argument('--saveID', type=str, default="",
                         help='Specify model save path.')
-    parser.add_argument('--max2keep', type=int, default=10,
+    parser.add_argument('--max2keep', type=int, default=100,
                         help='max checkpoints to keep')
     parser.add_argument('--batch_size', type=int, default=20,
                         help='Batch size.')
@@ -23,4 +23,12 @@ def parse_args():
                         help='Interval of evaluation.')
     parser.add_argument('--backbone', type=str, default='resnet34',
                         help='feature extractor backbone')
+    parser.add_argument('--best_epoch', type=int, default=0,
+                        help='the best epoch weight')
+    parser.add_argument('--resize', type=int, default=224,
+                        help='the best epoch weight')
+    parser.add_argument('--with_boundary', type=str, default='0',
+                        help='with boundary or not')
+    parser.add_argument('--o_dataset', type=str, default='layout_data_small',
+                        help='with boundary or not')
     return parser.parse_args()
