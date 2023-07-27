35763af270a42256b41464a890fe8e09ed72c53a
Sam Wei <68414598+Samwei1@users.noreply.github.com>
2023-07-27 14:56:59 +0800

    Update t.py

--- t.py
+++ t.py
@@ -1,30 +1,51 @@
 import numpy as np
 from scipy.spatial import Delaunay
 
+def point_in_polygon(point, polygon):
+    x, y = point
+    n = len(polygon)
+    inside = False
+
+    p1x, p1y = polygon[0]
+    for i in range(n + 1):
+        p2x, p2y = polygon[i % n]
+        if y > min(p1y, p2y):
+            if y <= max(p1y, p2y):
+                if x <= max(p1x, p2x):
+                    if p1y != p2y:
+                        xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
+                    if p1x == p2x or x <= xinters:
+                        inside = not inside
+        p1x, p1y = p2x, p2y
+
+    return inside
+
 def find_polygon_visual_center(polygon):
-    # 将多边形转换为numpy数组
-    polygon_points = np.array(polygon)
+    # 使用Delaunay三角剖分将凹多边形转化为多个三角形
+    tri = Delaunay(polygon)
 
-    # 执行多边形的三角剖分
-    triangulation = Delaunay(polygon_points)
+    # 存储所有三角形的重心坐标
+    triangle_centers = []
 
-    # 计算三角形的视觉中心并累加
-    center_x_sum = 0
-    center_y_sum = 0
-    num_triangles = triangulation.simplices.shape[0]
+    # 计算所有三角形的重心
+    for simplex in tri.simplices:
+        vertices = polygon[simplex]
+        center_x = np.mean(vertices[:, 0])
+        center_y = np.mean(vertices[:, 1])
+        triangle_centers.append((center_x, center_y))
 
-    for simplex in triangulation.simplices:
-        triangle_points = polygon_points[simplex]
-        triangle_center_x = np.mean(triangle_points[:, 0])
-        triangle_center_y = np.mean(triangle_points[:, 1])
-        center_x_sum += triangle_center_x
-        center_y_sum += triangle_center_y
+    # 计算所有三角形的重心的平均值，得到凹多边形的视觉中心
+    center_x = np.mean([center[0] for center in triangle_centers])
+    center_y = np.mean([center[1] for center in triangle_centers])
+    
+    visual_center = (center_x, center_y)
 
-    # 计算多边形的视觉中心（三角形视觉中心的平均值）
-    center_x = center_x_sum / num_triangles
-    center_y = center_y_sum / num_triangles
+    # 判断视觉中心是否在凹多边形内部，如果不在，则向凹多边形内部移动，直到找到一个在内部的点
+    while not point_in_polygon(visual_center, polygon):
+        for i in range(len(polygon)):
+            visual_center = ((visual_center[0] + polygon[i][0]) / 2, (visual_center[1] + polygon[i][1]) / 2)
 
-    return center_x, center_y
+    return visual_center
 
 # Example usage:
 polygon = [(0, 0), (4, 0), (4, 3), (2, 5), (0, 3)]  # Replace with your concave polygon's vertices
